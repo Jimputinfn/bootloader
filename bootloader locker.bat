e7e5bb313021553c681496f5d8d666f8ed0dd37f
Jimputinfn <89787903+Jimputinfn@users.noreply.github.com>
2023-09-28 19:31:31 +0300

    Add files via upload

---
+++ bootloader locker.bat
@@ -0,0 +1,67 @@
+Title Bootloader Locker Made By Jimputin
+echo off
+cls
+echo Welcome To Bootloader Locker!
+echo This tool will lock your bootloader
+echo WARNING THIS REMOVES ALL DATA FROM YOUR PHONE
+echo Do YOu Want To Continue
+pause
+echo Are You Sure?
+pause
+echo Are Your 100% Sure?
+pause
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+cls
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+cls
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+cls
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+cls
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+cls
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+cls
+cls
+echo Oki I am now locking your bootloader please wait.
+cls
+echo Oki I am now locking your bootloader please wait..
+cls
+echo Oki I am now locking your bootloader please wait...
+adb reboot bootloader
+fastboot flashing lock
+fastboot oem lock
+oem relock
+echo ALL DONE!
+echo YOU CAN NOW EXIT
+cls
---
+++ bootloader opener.bat
@@ -0,0 +1,34 @@
+title Android Bootloader Opener Made By Jimputin
+echo off
+cd c:/platform-tools
+cls
+echo Welcome To Bootloader Opener
+echo IF YOU DID NOT ALREADY TRHOW platform-tools FOLDER ALREADY ON YOUR C DRIVE DO IT NOW
+echo AND DONT CHANGE THE FOLDER NAME
+pause
+
+echo WHEN YOU GET INTO THE BOOTLOADER PRESS VOLUME DOWN AND SELECT OPEN BOOTLOADER OR SOMETHING THAT
+pause
+
+echo ARE YOU READY???
+pause
+
+cls
+adb devices
+echo do you see your phone on this list???
+echo IF NOT GO TO YOUR DEVELOPER SETTING AND ENABLE USB DEBUGGING AND OEM UNLOCKING
+echo AND RESTART THIS!
+pause
+
+echo WARNING THIS REMOVES ALL DATA FROM YOUR PHONE!!!!!!!!!!!!!!
+pause
+echo ARE YOU SURE THAT YOU WANT TO CONTINUE
+cls
+pause
+
+adb reboot bootloader
+fastboot flashing unlock
+fastboot oem unlock
+Echo ALL DONE NOW!!
+Echo RUN BOOTLOADER LOCKED IN YOU WANT TO LOCK YOUR BOOTLOADER AGAIN
+pause 
